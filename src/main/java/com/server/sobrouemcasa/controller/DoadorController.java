de0ed35b11e91e5e4ec8eae4e96a06ca20521618
M4teusPrado <mateus.silvaprado@gmail.com>
2022-09-22 13:53:29 -0300

    SEC-15 - Delete doador

--- src/main/java/com/server/sobrouemcasa/controller/DoadorController.java
+++ src/main/java/com/server/sobrouemcasa/controller/DoadorController.java
@@ -2,6 +2,7 @@ package com.server.sobrouemcasa.controller;
 
 import com.server.sobrouemcasa.service.DoadorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,5 +11,12 @@ public class DoadorController {
 
     @Autowired
     private DoadorService doadorService;
+
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteDoador(@PathVariable Long id) {
+        doadorService.deleteDoador(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
