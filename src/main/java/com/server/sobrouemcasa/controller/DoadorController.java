acaf797ab4823aea40a09117f32d482a3efb6055
M4teusPrado <mateus.silvaprado@gmail.com>
2022-09-28 21:26:17 -0300

    hotfix - ajuste develop

--- src/main/java/com/server/sobrouemcasa/controller/DoadorController.java
+++ src/main/java/com/server/sobrouemcasa/controller/DoadorController.java
@@ -1,7 +1,9 @@
 package com.server.sobrouemcasa.controller;
 
 import com.server.sobrouemcasa.dto.DoadorDTO;
+import com.server.sobrouemcasa.model.Doacao;
 import com.server.sobrouemcasa.model.Doador;
+import com.server.sobrouemcasa.service.DoacaoService;
 import com.server.sobrouemcasa.service.DoadorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -32,6 +34,4 @@ public class DoadorController {
     public ResponseEntity<Doador> updateDoador(@PathVariable Long id, @RequestBody DoadorDTO doador) {
         return ResponseEntity.ok().body(doadorService.updateDoador(id,doador));
     }
-
-
 }
